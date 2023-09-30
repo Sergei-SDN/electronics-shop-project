8b94d4516da23b097ff4d0c9ed128760ae0f93e1
Sergei <you@example.com>
2023-09-30 15:40:40 +0800

    fix homework-4

--- src/phone.py
+++ src/phone.py
@@ -1,7 +1,13 @@
 from src.item import Item
+
+
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
+        if not isinstance(number_of_sim, int):
+            raise ValueError('Значение должно быть целым числом.')
+        elif number_of_sim <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
         self._number_of_sim = number_of_sim
 
     def __repr__(self):
